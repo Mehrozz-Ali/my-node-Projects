fb5ca3d53456b76079d7c2d9411ccc14a66aadde
Mehrozz-Ali <mehrozali912@gmail.com>
2025-10-22 21:25:44 -0700

    prepare server for railway

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(express.json()); // this is used when we want to render a frontend data 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log('Database connection error:', err));
+    
 
 
 app.get('/', (req, res) => {
